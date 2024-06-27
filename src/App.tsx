15d5ad0cff878e176c1dd7473e2e859c91382012
Khoa Vu <khoa.vu@kyanon.digital>
2024-06-27 11:53:46 +0700

    fix build failed

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { generateClient } from "aws-amplify/data";
 
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-import { signOut } from "aws-amplify/auth";
-
 
 
 const client = generateClient<Schema>();
